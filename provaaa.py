6636d6c9314164c5bf0ad632f66fedad985e03f0
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2021-04-13 19:47:58 -0300

    Add files via upload

---
+++ provaaa.py
@@ -0,0 +1,26 @@
+#Crie uma função que retorn o maior número ímpar da sequencia (sem usar max). 
+
+#Ex: fn([1,2,3,4,5,6,7,19,8,9,10,11,12]) -> 19
+
+maior = 0 
+memor= 0
+for c in range(1, 14):
+ num = int(input('digite um numero')
+  
+  if num % 2 == 0
+    maior  maior 
+
+  else 
+    menor += menor 
+
+ print(f\'O maior é {maior}') 
+
+
+ 2) Crie uma função que retorna todas as palavras de uma string em ordem reversa: 
+
+Ex: fn("A frase inteira deve ser revertida") -> "revertida ser deve inteira frase a"
+
+n =str(input('Digite seu nome completo ')
+nome = n.split()
+def reversed_string(a_string):
+print('seu nome revertido'.format(nomelend(n) 
